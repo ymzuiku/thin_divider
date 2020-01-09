f525eb4ecf569d6fdd2b163e2f2406bffa3dd90e
梁柱 <ymblender@gmail.com>
2020-01-09 09:21:07 +0800

    1

---
+++ README.md
@@ -0,0 +1,7 @@
+# thin_divider
+
+Divider not show in browser, thin_divider is compatible web browser
+
+```dart
+ThinDivider()
+```
---
+++ lib/thin_divider.dart
@@ -0,0 +1,21 @@
+library thin_divider;
+
+import 'package:flutter/material.dart';
+
+class ThinDivider extends StatelessWidget {
+  final double height;
+  final double width;
+  final double color;
+
+  ThinDivider({this.height, this.width, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color ?? Theme.of(context).dividerColor,
+      height: height ?? 0.5,
+      width: width ?? double.infinity,
+      child: SizedBox(),
+    );
+  }
+}
